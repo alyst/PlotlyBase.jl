2577534f416c5e1eb0b1922fdb9165f6187f3fed
Alexey Stukalov <astukalov@gmail.com>
2018-02-02 16:36:11 +0000

    handle_titles!() -> add_subplot_annotation!()

--- src/subplots.jl
+++ src/subplots.jl
@@ -42,7 +42,7 @@ end
 hastitle(layout::Layout) = haskey(layout.fields, "title") || haskey(layout.fields, :title)
 hastitle(plot::Plot) = hastitle(plot.layout)
 
-function handle_titles!(big_layout, sub_layout, ix::Int)
+function add_subplot_annotation!(big_layout::Layout, sub_layout::Layout, ix::Integer)
     hastitle(sub_layout) || return big_layout
 
     # check for symbol or string
@@ -81,7 +81,7 @@ function _cat(nr::Int, nc::Int, ps::Plot...)
 
     for col in 1:nc, row in 1:nr
         ix = LinearIndices((nc, nr))[col, row]
-        handle_titles!(layout, copied_plots[ix].layout, ix)
+        add_subplot_annotation!(layout, copied_plots[ix].layout, ix)
         layout["xaxis$ix"] = merge(copied_plots[ix].layout["xaxis"], layout["xaxis$ix"])
         layout["yaxis$ix"] = merge(copied_plots[ix].layout["yaxis"], layout["yaxis$ix"])
 
